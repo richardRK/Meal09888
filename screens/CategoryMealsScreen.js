3288061a91b55f8c418c88fb4a2abde84ac8b67c
rohi <richardRK@users.noreply.github.com>
2021-03-21 23:07:33 +0530

    Navigate to pages usign stack

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 
-const CategoryMealScreen = props => {
+const CategoryMealScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text>The Category Meal Screen!</Text>
+
+      <Button
+        title="Go to Meal Details!"
+        onPress={() => {
+          props.navigation.navigate({ routeName: "MealDetail" });
+        }}
+      ></Button>
     </View>
   );
 };
@@ -12,9 +19,9 @@ const CategoryMealScreen = props => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default CategoryMealScreen;
