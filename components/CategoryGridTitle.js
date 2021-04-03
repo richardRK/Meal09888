f5cd9e9d6318af3928ea3f13bfa54c14c9a39872
rohi <richardRK@users.noreply.github.com>
2021-04-03 19:58:14 +0530

    t3

--- components/CategoryGridTitle.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { TochableOpacity, View, Text, StyleSheet } from "react-native";
-
-const CategoryGridTitle = (props) => {};
-
-const styles = StyleSheet.create({});
-
-export default CategoryGridTitle;
